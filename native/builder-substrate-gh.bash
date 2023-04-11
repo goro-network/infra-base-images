123e4d7831d72c635a050a198f88e4a7eb3280be
Aditya Kresna <aditya.kresna@outlook.co.id>
2023-04-11 10:09:09 +0700

    feat: unify multiarch builder substrate (#5)
    * feat(native): unify multiarch builder substrate
    
    * feat(native): push more features for x64
    
    * feat(native): use dynamic registration token with bearer

--- native/builder-substrate-gh.bash
+++ native/builder-substrate-gh.bash
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-RUNNER_GROUP=${RUNNER_GROUP}
-RUNNER_LABELS=${RUNNER_LABELS}
-RUNNER_NAME=${RUNNER_NAME}
-RUNNER_ORG=${RUNNER_ORG}
-RUNNER_TOKEN=${RUNNER_TOKEN}
+GH_AUTH_TOKEN=${GH_AUTH_TOKEN}
+GH_ORGANIZATION=${GH_ORGANIZATION}
+GH_RUNNER_GROUP=${GH_RUNNER_GROUP}
+GH_RUNNER_LABELS=${GH_RUNNER_LABELS}
+GH_RUNNER_NAME=${GH_RUNNER_NAME}
+
+function get_github_runner_registration_token() {
+    curl -sX POST https://api.github.com/orgs/${GH_ORGANIZATION}/actions/runners/registration-token \
+        -H "Authorization: Bearer ${GH_AUTH_TOKEN}" | jq .token --raw-output
+}
 
 ./config.sh \
     --disableupdate \
     --replace \
     --unattended \
-    --labels ${RUNNER_LABELS} \
-    --name ${RUNNER_NAME} \
-    --runnergroup ${RUNNER_GROUP} \
-    --token ${RUNNER_TOKEN} \
-    --url "https://github.com/${RUNNER_ORG}"
+    --labels ${GH_RUNNER_LABELS} \
+    --name ${GH_RUNNER_NAME} \
+    --runnergroup ${GH_RUNNER_GROUP} \
+    --token $(get_github_runner_registration_token) \
+    --url "https://github.com/${GH_ORGANIZATION}"
 
 cleanup_runner() {
     echo "Removing runner..."
     ./config.sh \
         remove \
-        --token ${RUNNER_REG}
+        --token $(get_github_runner_registration_token)
 }
 
 trap 'cleanup_runner; exit 130' SIGINT
